3e198610013516056209251c26d682f0c530fc18
meonlol <mail@leonmoll.de>
2015-04-13 08:40:29 +0200

    minor refactoring

--- plugin/find_paths/path_file_manager.rb
+++ plugin/find_paths/path_file_manager.rb
@@ -3,36 +3,40 @@
 class PathFileManager
 
 	def self.convertOutputResultToSources()
-		if not File.exists?(ProjectControler::GRADLE_WRITE_FILE)
-			return
+		if File.exists?(ProjectControler::GRADLE_WRITE_FILE)
+			pathsString = IO.readlines(ProjectControler::GRADLE_WRITE_FILE)[0]
+			paths = pathsString.split(':')
+
+			appendPathsToSources(paths)
+
+			File.delete(ProjectControler::GRADLE_WRITE_FILE)
 		end
+	end
 
-		pathsString = IO.readlines(ProjectControler::GRADLE_WRITE_FILE)[0]
-		paths = pathsString.split(':')
+	def self.appendPathsToSources(paths)
 		appending = false
-
 		if File.exists?(ProjectControler::LIBRARY_PATHS_FILE)
 			appending = true
 			definedPaths = IO.readlines(ProjectControler::LIBRARY_PATHS_FILE)
 
 			removeDuplicatePathEntries(paths, definedPaths)
-
-			definedPaths.last << "\n"
 		end
 
+		reformatRawPaths(paths)
+
+		File.open(ProjectControler::LIBRARY_PATHS_FILE, 'a') { |file|
+			if appending
+				file.write("\n") # Start on a new line
+			end
+			file.write(paths.join)
+		}
+	end
+
+	def self.reformatRawPaths(paths)
 		paths.each { |path|
 			path.prepend "+ "
 			path.concat "\n"
 		}
-
-
-		File.open(ProjectControler::LIBRARY_PATHS_FILE, 'a') { |file|
-			if appending
-				file.write("\n")
-			end
-			file.write(paths.join)
-		}
-		File.delete(ProjectControler::GRADLE_WRITE_FILE)
 	end
 
 	def self.removeDuplicatePathEntries(source, duplicates)
